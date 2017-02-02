da35615cab9e31c8357a7f2f06229288e0a9603b
Sergio Daniel Moreno <sergmor@gmail.com>
2017-02-02 18:50:22 -0500

    first commit

---
+++ build.sbt
@@ -0,0 +1,13 @@
+name := """activator-ftp"""
+
+version := "1.0"
+
+scalaVersion := "2.11.6"
+
+libraryDependencies ++= Seq(
+  // Uncomment to use Akka
+  //"com.typesafe.akka" % "akka-actor_2.11" % "2.3.9",
+  "commons-net" % "commons-net" % "3.5",
+  "junit"             % "junit"           % "4.12"  % "test",
+  "com.novocode"      % "junit-interface" % "0.11"  % "test"
+)
---
+++ src/main/java/com/example/Hello.java
@@ -0,0 +1,55 @@
+package com.example;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPClientConfig;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.IOException;
+
+class Hello {
+    public static void main(String[] args) {
+
+        FTPClient ftp = new FTPClient();
+        FTPClientConfig config = new FTPClientConfig();
+        //config.setXXX(YYY); // change required options
+        // for example config.setServerTimeZoneId("Pacific/Pitcairn")
+        ftp.configure(config );
+        boolean error = false;
+        try {
+            int reply;
+            String server = "10.0.14.22";
+            ftp.connect(server);
+            ftp.login("transactions", "-KQhJK5,=B");
+            System.out.println("Connected to " + server + ".");
+            System.out.print(ftp.getReplyString());
+
+            // After connection attempt, you should check the reply code to verify
+            // success.
+            reply = ftp.getReplyCode();
+
+            if(!FTPReply.isPositiveCompletion(reply)) {
+                ftp.disconnect();
+                System.err.println("FTP server refused connection.");
+                System.exit(1);
+            }
+
+            System.out.println("++Connected!!");
+
+            ftp.logout();
+        } catch(IOException e) {
+            error = true;
+            e.printStackTrace();
+        } finally {
+            if(ftp.isConnected()) {
+                try {
+                    ftp.disconnect();
+                } catch(IOException ioe) {
+                    // do nothing
+                }
+            }
+            System.exit(error ? 1 : 0);
+        }
+
+
+    }
+}
