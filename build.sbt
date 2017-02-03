03b10cb7fa5d75f79422cecc6a7f27ce7f6d3e3a
Sergio Daniel Moreno <sergmor@gmail.com>
2017-02-02 19:34:22 -0500

    sftp instead

--- build.sbt
+++ build.sbt
@@ -8,6 +8,9 @@ libraryDependencies ++= Seq(
   // Uncomment to use Akka
   //"com.typesafe.akka" % "akka-actor_2.11" % "2.3.9",
   "commons-net" % "commons-net" % "3.5",
+  "org.apache.commons" % "commons-vfs2" % "2.1",
+  "com.jcraft" % "jsch" % "0.1.54",
+  "commons-logging" % "commons-logging" % "1.2",
   "junit"             % "junit"           % "4.12"  % "test",
   "com.novocode"      % "junit-interface" % "0.11"  % "test"
 )
--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -3,11 +3,98 @@ package com.example;
 import org.apache.commons.net.ftp.FTPClientConfig;
 import org.apache.commons.net.ftp.FTPReply;
 import org.apache.commons.net.ftp.FTPSClient;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemOptions;
+import org.apache.commons.vfs2.Selectors;
+import org.apache.commons.vfs2.impl.StandardFileSystemManager;
+import org.apache.commons.vfs2.provider.sftp.SftpFileSystemConfigBuilder;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 
 class Hello {
+
+    static Properties props;
+
     public static void main(String[] args) {
+
+        Hello sendMyFiles = new Hello();
+
+        sendMyFiles.startFTP("", "upload.txr");
+
+    }
+
+    public boolean startFTP(String propertiesFilename, String fileToFTP){
+
+        props = new Properties();
+        StandardFileSystemManager manager = new StandardFileSystemManager();
+
+        try {
+
+            //props.load(new FileInputStream("properties/" + propertiesFilename));
+//            String serverAddress = props.getProperty("serverAddress").trim();
+//            String userId = props.getProperty("userId").trim();
+//            String password = props.getProperty("password").trim();
+//            String remoteDirectory = props.getProperty("remoteDirectory").trim();
+//            String localDirectory = props.getProperty("localDirectory").trim();
+
+
+
+            String serverAddress =  "10.0.14.22";
+            String userId = "transactions";
+            String password = "-KQhJK5,=B";
+            String remoteDirectory = "test";
+            String localDirectory = "./";
+
+
+            //check if the file exists
+            String filepath = localDirectory +  fileToFTP;
+
+            File file = new File(filepath);
+            if (!file.exists())
+                throw new RuntimeException("Error. Local file not found");
+
+            //Initializes the file manager
+            System.out.println("will try uploading "+ filepath);
+            manager.init();
+
+            //Setup our SFTP configuration
+            FileSystemOptions opts = new FileSystemOptions();
+            SftpFileSystemConfigBuilder.getInstance().setStrictHostKeyChecking(
+                    opts, "no");
+            SftpFileSystemConfigBuilder.getInstance().setUserDirIsRoot(opts, true);
+            SftpFileSystemConfigBuilder.getInstance().setTimeout(opts, 10000);
+
+            //Create the SFTP URI using the host name, userid, password,  remote path and file name
+            String sftpUri = "sftp://" + userId + ":" + password +  "@" + serverAddress + "/" +
+                    remoteDirectory + fileToFTP;
+
+            // Create local file object
+            FileObject localFile = manager.resolveFile(file.getAbsolutePath());
+            System.out.println("about to connect...");
+            // Create remote file object
+            FileObject remoteFile = manager.resolveFile(sftpUri, opts);
+
+            // Copy local file to sftp server
+            remoteFile.copyFrom(localFile, Selectors.SELECT_SELF);
+            System.out.println("File upload successful");
+
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+        finally {
+            manager.close();
+        }
+
+        return true;
+    }
+
+
+
+    public static void testMain(String[] args) {
 
         FTPSClient ftp = new FTPSClient();
         FTPClientConfig config = new FTPClientConfig();
